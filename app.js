c6cccf1ce8d1f37b185311ccfb9bdabdb07894e9
NANDHINI MB <nandhini.mb@mantralabsglobal.com>
2020-03-28 17:09:00 +0530

    upload video

--- app.js
+++ app.js
@@ -7,23 +7,39 @@ const multer =require('multer');
 const GridFsStorage = require('multer-gridfs-storage');
 const Grid= require('gridfs-stream');
 const methodOverride = require('method-override');
-
+const flash = require('connect-flash');
+//const {check,validationResult} = require('express-validator');
+const expressValidator = require('express-validator');
+const session = require('express-session');
 const app = express();
 
 //Middleware
-app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.set('view engine','ejs')
 
+app.use(bodyParser.json());
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
+app.use(expressValidator())
+app.use(methodOverride('_method'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs')
 //Mongo URI
 const mongoURI = 'mongodb://127.0.0.1:27017/mongouploads';
 //Create connection
 const conn= mongoose.createConnection(mongoURI);
 let gfs;
 conn.once('open',() => {
+	console.log("connected to mongodb");
 	gfs=Grid(conn.db,mongoose.mongo);
 	gfs.collection('uploads');
+	gfs.collection('User');
 })
+
+
+conn.on('error',function(err){
+	console.log(err);
+});
 
 //create storage engine
 const storage = new GridFsStorage({
@@ -45,16 +61,115 @@ const storage = new GridFsStorage({
   }
 });
 const upload = multer({ storage })
-app.get('/',(req,res) => {
 
-	res.render('index');
 
-});
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+
 app.post('/upload', upload.single('file'), (req, res) => {
   //res.json({ file: req.file });
   res.redirect('/');
 });
 
+
+// app.get('/login',function(req,res){
+//     res.render('login');
+//   });
+
+
+
+// app.post('/login', [
+//   check('email', 'email is required').isEmail(),
+//   check('password', 'password is required').not().isEmpty(),
+//   ], function(req, res, next) {
+//   //check validate data
+
+//   const result= validationResult(req);
+//   var errors = result.errors;
+//   for (var key in errors) {
+//         console.log(errors[key].value);
+//   }
+//   if (!result.isEmpty()) {
+//      res.render('login', {
+//       errors: errors
+//     });
+//  }
+  // else{
+  //   var email =req.body.email; 
+  //   var pass = req.body.password; 
+
+  //   conn.collection('signup').find({
+  //       "email": email,"password":pass
+  //   }).toArray((err,result)=>{
+  //   if(!result || result.length == 0){
+		// 	return res.status(404).json({
+		// 		err:'No user Exist'
+		// 	});
+		// }
+    
+  //   else
+  //    {
+  //    	res.redirect('/');
+  //    }
+  //   });
+  //   }
+  // res.redirect('/');	
+  // });
+   
+
+
+// app.get('/signup',function(req,res){
+//     res.render('signup');
+//   });
+
+
+// app.post('/signup', function(req,res){ 
+//     // var name = req.body.name;
+//     //console.log(req.body.email) 
+//     var email =req.body.email; 
+//     var pass = req.body.password; 
+//     // var phone =req.body.phone; 
+  
+//     var data = { 
+//         "email":email, 
+//         "password":pass, 
+//     } 
+
+// conn.collection('signup').insertOne(data,function(err, collection){ 
+//         if (err) throw err; 
+//         console.log("Record inserted Successfully"); 
+              
+//     });         
+//     return res.redirect('login'); 
+// });
+
+app.get('/',(req,res) => {
+    var query_video={contentType : 'video/mp4'};
+	gfs.files.find(query_video).toArray((err,files)=>{
+		if(!files || files.length == 0){
+		     res.render('index',{files:false});	
+		}
+		else
+		{
+			res.render('index',{files:files});
+		}
+		//return res.json(files);
+	});
+
+});
+
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});	
 // @route GET /files
 app.get('/files',(req,res)=>{
 	gfs.files.find().toArray((err,files)=>{
@@ -99,6 +214,18 @@ app.get('/video/:filename',(req,res)=>{
 		}
 	});
 });
+
+app.delete('/files/:id',(req,res) =>{
+	gfs.remove({_id:req.params.id, root: 'uploads'},(err,gridStore)=>{
+		if(err){
+			return res.status(404).json({err:'No file to delete'});
+		}
+		res.redirect('/');
+    });
+});
+//const User=require('./models/User');
+let users = require('./routes/user');
+app.use('/users', users);
 
 const port = 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ routes/user.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+
+// Bring in User Model
+let User = require('../models/User');
+
+// Register Form
+router.get('/signup', function(req, res){
+  res.render('signup');
+});
+
+// Register Proccess
+router.post('/signup', function(req, res){
+  const uname = req.body.uname;
+  const email = req.body.email; 
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('uname', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  //req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+  let errors = req.validationErrors();
+  if(errors){
+    res.render('signup', {
+      errors:errors
+    });
+  } 
+  else {
+    
+    let newUser = new User({
+      uname:uname,
+      email:email,
+      password:password,
+      password2:password2
+    });
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(newUser.password, salt, function(err, hash){
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save(function(err){
+          console.log("new user save");
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            console.log("save");
+            req.flash('success','You are now registered and can log in');
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+
+    // console.log(newUser);
+    // req.flash('success','You are now registered and can log in');
+    // res.redirect('/users/login');
+  }
+});
+
+//Login Form
+router.get('/login', function(req, res){
+res.render('login');
+});
+
+// // Login Process
+// router.post('/login', function(req, res, next){
+//   passport.authenticate('local', {
+//     successRedirect:'/',
+//     failureRedirect:'/users/login',
+//     failureFlash: true
+//   })(req, res, next);
+// });
+
+// // logout
+// router.get('/logout', function(req, res){
+//   req.logout();
+//   req.flash('success', 'You are logged out');
+//   res.redirect('/users/login');
+// });
+
+module.exports = router;
